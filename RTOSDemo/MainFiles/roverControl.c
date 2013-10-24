186faa7b4c9700e1866d07e82980d364fab24833
Alex Mosolgo <amosolgo@gmail.com>
2013-10-24 13:01:56 -0400

    COMPILED BUT NOT TESTED.  Updated project file to include missing files.  Added partial implementation of queue in rover control task.

--- RTOSDemo/MainFiles/roverControl.c
+++ RTOSDemo/MainFiles/roverControl.c
@@ -1,5 +1,6 @@
 #include "roverControl.h"
 #include "public_messages.h"
+#include "uartDriver.h"
 
 #define roverControlQLen (10)
 
@@ -24,15 +25,40 @@ void startRoverControlTask(RoverControlStruct *roverControlData, unsigned portBA
 }
 
 static portTASK_FUNCTION( roverControlTask, param ) {
+	// Get the parameters
+	RoverControlStruct *roverControlData = (RoverControlStruct *) param;
+	// Variable to hold received messages
+	public_message_t receivedMsg;
+
+	// Sensor data request message
+#warning "Still using old UARTmsg struct"
+	UARTmsg sensorRequestMsg;
+	sensorRequestMsg.msgType = PUB_MSG_T_SENS_DIST;
+	sensorRequestMsg.msgID = 0; // The count will be updated before sending each request
+	sensorRequestMsg.txLen = 0; // No data is included in the request
+	sensorRequestMsg.data = NULL; // No data is included in the request
+
 	// Request sensor data
+	// Update the count and send the request
+	sensorRequestMsg.msgID = public_message_get_count(PUB_MSG_T_SENS_DIST);
+	uartEnQ(roverControlData->uartDevice, sensorRequestMsg.msgType, sensorRequestMsg.msgID, sensorRequestMsg.txLen,
+		sensorRequestMsg.data);
 
 	for(;;)
 	{
 		// 1. Read sensor data response from the conductor (blocks until
 		// data is available).
+		if (xQueueReceive(roverControlData->inQ, (void *) &receivedMsg, portMAX_DELAY) != pdTRUE) {
+			VT_HANDLE_FATAL_ERROR(0);
+		}
 
-		// 2. Request new sensor data
+		// 2. Request new sensor data.
+		// Update the count and send the request
+		sensorRequestMsg.msgID = public_message_get_count(PUB_MSG_T_SENS_DIST);
+		uartEnQ(roverControlData->uartDevice, sensorRequestMsg.msgType, sensorRequestMsg.msgID, sensorRequestMsg.txLen,
+			sensorRequestMsg.data);
 
-		// 3. Process data received in step 1, do task stuff
+		// 3. Process message received in step 1, contained in receivedMsg.
+		// See public_messages.h for the structure of receivedMsg (it is type "public_message_t").
 	}
 }
