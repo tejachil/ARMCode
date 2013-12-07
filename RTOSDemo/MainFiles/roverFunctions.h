49a4c86c011f0c0cb909a13ea58aec5a2099a6f4
Teja Chiluvuri <tejachil@vt.edu>
2013-12-07 15:38:35 -0500

    Implemented the moveRoverDist function but it is untested.

--- RTOSDemo/MainFiles/roverFunctions.h
+++ RTOSDemo/MainFiles/roverFunctions.h
@@ -3,6 +3,7 @@
 #include "roverHeader.h"
 
 void moveRover(RoverControlStruct *roverControlData);
+void moveRoverDist(RoverControlStruct *roverControlData, uint8_t revs, uint16_t ticksOffset);
 void stopRover(RoverControlStruct *roverControlData);
 void turnRover(RoverControlStruct *roverControlData, uint8_t angle);
 void fixRover(RoverControlStruct *roverControlData, int fixDir);
