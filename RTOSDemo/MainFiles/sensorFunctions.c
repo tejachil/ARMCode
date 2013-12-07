39dc59a81f36f6e5a1fc1be6bb324179bd86b252
Teja Chiluvuri <tejachil@vt.edu>
2013-12-07 18:34:28 -0500

    Implemented the distanceToEncoder method but have not tested yet. Hoping this will fix the problem of going strait after turn.

--- RTOSDemo/MainFiles/sensorFunctions.c
+++ RTOSDemo/MainFiles/sensorFunctions.c
@@ -133,5 +133,7 @@ double getEncoderDistance(uint8_t revolutions, uint16_t ticksOffset){
 }
 
 void distanceToEncoder(double distance, uint8_t* revolutions, uint16_t* ticksOffset){
-
+	distance = distance/WHEEL_CIRCUMFERENCE;
+	*revolutions = (uint8_t)(distance);
+	*ticksOffset = (uint16_t)((distance - revolutions)*TICKS_PER_REVOLUTION);
 }
